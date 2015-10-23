f718f0b90190e56826824db6bd12d47fb74b8846
Max Miroff <maxmiroff@gmail.com>
2015-10-23 18:17:04 -0400

    Added Facebook integration.

---
+++ meteor-app/server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '1649191962026500',
+    secret: 'c8ed8a84e5f5044db0bc0263cc9d142e'
+});
