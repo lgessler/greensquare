a16e069087fbeeec8e4b2812369de2c84b4b0ce6
Max Miroff <maxmiroff@gmail.com>
2015-10-23 19:28:23 -0400

    Added map functionality to each space page + markers display green spaces on the map.

--- meteor-app/client/templates/spaces/space_page.js
+++ meteor-app/client/templates/spaces/space_page.js
@@ -1,5 +1,35 @@
 Template.spacePage.helpers({
   reviews: function() {
     return Reviews.find({spaceId: this._id});
+  },
+
+  lat: function() {
+    return this.latitude;
+  },
+
+  lon: function() {
+    return this.longitude;
+  },
+
+  mapOptions: function() {
+    if (GoogleMaps.loaded()) {
+      return {
+        center: new google.maps.LatLng(this.latitude, this.longitude),
+        zoom: 15,
+      };
+    }
   }
+});
+
+Template.body.onCreated(function() {
+  // We can use the `ready` callback to interact with the map API once the map is ready.
+  GoogleMaps.ready('map', function(map) {
+    // Add a marker to the map once it's ready
+    var marker = new google.maps.Marker({
+      position: map.options.center,
+      map: map.instance,
+      icon: '/siteAssets/gsmarker40.png',
+      title: this.title
+    });
+  });
 });
