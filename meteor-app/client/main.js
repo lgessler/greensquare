659a4a14aaa084e91340826cb6dfc60fec456a41
Luke Gessler <lukegessler@gmail.com>
2015-10-23 15:58:59 -0400

    removed reference to old package

--- meteor-app/client/main.js
+++
@@ -1,8 +0,0 @@
-// Initialize isotope, the package we use to make the pretty grid layout.
-$('.grid').isotope({
-  // options...
-  itemSelector: '.grid-item',
-  masonry: {
-    columnWidth: 200
-  }
-});
