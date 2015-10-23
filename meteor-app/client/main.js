03d55fbac8c741bd197e2f3b3bdc167fd243c0d2
Luke Gessler <lukegessler@gmail.com>
2015-10-23 15:43:26 -0400

    First steps towards implementing a grid

---
+++ meteor-app/client/main.js
@@ -0,0 +1,8 @@
+// Initialize isotope, the package we use to make the pretty grid layout.
+$('.grid').isotope({
+  // options...
+  itemSelector: '.grid-item',
+  masonry: {
+    columnWidth: 200
+  }
+});
