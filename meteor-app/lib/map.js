eee00b9d86be41a7bcc97e3c4b4ab47c2621e235
kdko <kdko@email.wm.edu>
2015-10-24 09:31:29 -0400

    Merge branch 'master' of https://github.com/lgessler/greensquare
    # Conflicts:
    #	meteor-app/client/templates/spaces/space_item.html

--- meteor-app/lib/map.js
+++ meteor-app/lib/map.js
@@ -1,5 +1,13 @@
 if (Meteor.isClient) {
     Meteor.startup(function() {
         //GoogleMaps.load();
+
+    if (Session.get('lat') == undefined
+        || Session.get('lon') == undefined) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            Session.set('lat', position.coords.latitude);
+            Session.set('lon', position.coords.longitude);
+        });
+    }
     }); 
 }
